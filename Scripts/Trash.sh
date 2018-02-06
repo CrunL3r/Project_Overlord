8f12e389eda1be3eb0f25da55d18acf1730651f9
and3rx <Avalenciacs@gmail.com>
2018-02-06 22:53:32 +0100

    Update trash3

--- Scripts/Trash.sh
+++ Scripts/Trash.sh
@@ -1,23 +1,20 @@
 # Creador por Anderson valencia castaño
 #archivos borrados sin modificacion en 20 dias
+
+#Librerias
 . ${pathsmta}/includes/Lib/control.sh
+#Ubicacion papelera
 configs bin MvTrash
+#Ubicacion directorio usuario
+configs directory UserDir
 
-#Detectar el home del usuario
-Userhome=$(pwd)
 #Crear en el usuario la carpeta con los archivos
-mkdir -m 777 $Userhome/Escritorio/Archivos
-DirArch=$Userhome/Escritorio/Archivos
 
-echo -n "Quieres crear la ruta de la papelera?"
-echo -n "introduce 1 para si"
-echo -n "introduce 2 para no"
-read seleccion1
 
-if ($seleccion1 == 1){
-	echo -n "introduce la ruta de la papelera"
-	read papelera1
-	find $DirArch -maxdepth 1 -atime +20 -type f -exec mv "{}" $papelera1
+
+
+if (-d $MvTrash ){
+	find $UserDir -maxdepth 1 -atime +20 -type f -exec mv "{}" $MvTrash
 }
 then
 
@@ -30,10 +27,12 @@ else
 fi
 
 
-if ($seleccion1 == 2){
-	mkdir -m 777 $Userhome/Escritorio/Papelera
-	DirTrash=$Userhome/Escritorio/Papelera
-	find $DirArch -maxdepth 1 -atime +20 -type f -exec mv "{}" $DirTrash
+if (! -d $MvTrash){
+	echo 'The trash dont exist'
+	echo 'Select where you want the trash'
+	read Newtrash
+	mkdir -m 777 $Newtrash
+	find $UserDir -maxdepth 1 -atime +20 -type f -exec mv "{}" $Newtrash
 }
 then
 

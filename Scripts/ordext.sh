a4d6e71a53e9114577aa5ba60fd7d3633b32f66c
Andreu <andreullabresb@gmail.com>
2018-02-06 23:45:30 +0100

    Update agrupar x extensión

--- Scripts/ordext.sh
+++ Scripts/ordext.sh
@@ -3,13 +3,15 @@
 function ordext(){
 if [[ ! -d /sk/ ]]; then
 	mkdir sk
-	# action sería donde se produce todo
-	ls -p action/ | grep -v / | cut -d . -f 2 | sort | uniq > sk/ext.txt
-	ls -p action/ | grep -v / | sort > sk/arc.txt
+	# Directorio action sería donde se produce todo
+	# Recoge las extensiones de los archivos
+	ls -p action/*.* | cut -d . -f 2 | sort | uniq > sk/ext.txt
+	# Recoge los nombres de los archivos con su extensión
+	ls -p action/*.* | cut -d / -f 2 |sort > sk/arc.txt
 	#Hacer un for anidado para sacar las extensiones 
 	#y después mover los archivos dependiendo de esa extension.
 	for i in `cat sk/ext.txt`
-	do
+	do	
 		count=0		
 		mkdir action/$i
 		for x in `cat sk/arc.txt`
@@ -26,9 +28,22 @@ if [[ ! -d /sk/ ]]; then
 			let count=count+1
 		done 
 	done
-	#Eliminar los archivos utilizados
-	rm -R sk
+	#Ver si quedan archivos existentes que no tengan extensión
+	ls -p action/ | grep -v / |sort > sk/arcsext.txt
+	# ESTA EN OBRAS!!!
+	# ===========================================
+	if [[ -f sk/arcext.txt ]]; then
+		pathu=action/unknown		
+		mkdir $pathu
+		for z in `cat sk/arcext.txt`	
+		do
+			mv action/$z $pathu/$z
+		done
+	fi
+	# ============================================
+	rm -r sk
 	unset count
+	unset pathu
 fi
 }
 ordext
